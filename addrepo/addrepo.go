a2eefbaa47e43baaea8d048abd2c70673271d82a
Beyang Liu <beyang@sourcegraph.com>
2020-07-08 21:02:07 -0700

    add repo script

---
+++ addrepo/addrepo.go
@@ -0,0 +1,50 @@
+package addrepo
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/google/go-github/github"
+)
+
+func Main() error {
+	const perPage = 100
+	inDir := "api_response_dump"
+	outDir := "added"
+	// languages := []string{"javascript", "java", "python", "php", "ruby", "c#", "c++", "c", "shell", "objective-c", "go", "swift", "scala", "rust", "kotlin"}
+
+	files, err := ioutil.ReadDir(inDir)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		// TODO: check for outfile
+
+		f, err := os.Open(filepath.Join(inDir, file.Name()))
+		if err != nil {
+			log.Printf("Error reading file %s: %s", file, err)
+			continue
+		}
+		defer f.Close()
+		var result github.RepositoriesSearchResult
+		if err := json.NewDecoder(f).Decode(&result); err != nil {
+			log.Printf("Error parsing file %s JSON: %s", file, err)
+			continue
+		}
+		for _, repo := range result.Repositories {
+			if repo.FullName == nil {
+				log.Printf("Repo missing FullName: %#v", repo)
+				continue
+			}
+			log.Printf("Full name: %s", *repo.FullName)
+		}
+		outFile := filepath.Join(outDir, file.Name())
+		if err := ioutil.WriteFile(outFile, []byte(""), 0644); err != nil {
+			log.Printf("Error writing verification file %s: %s", outFile, err)
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/beyang/ghdump/ghdump"
+import (
+	"log"
+	"os"
+
+	"github.com/beyang/ghdump/addrepo"
+	"github.com/beyang/ghdump/ghdump"
+)
 
 func main() {
+	if len(os.Args) >= 2 && os.Args[1] == "add" {
+		if err := addrepo.Main(); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
 	ghdump.Main()
 }
