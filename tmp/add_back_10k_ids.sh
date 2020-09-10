6fab852c4fcd7a12fc1f9c5d2a5b333067caa47c
Beyang Liu <beyang@sourcegraph.com>
2020-09-09 18:54:24 -0700

    10k_ids_pg2; fix script

--- tmp/add_back_10k_ids.sh
+++ tmp/add_back_10k_ids.sh
@@ -1,1 +1,2 @@
-cat 10k_ids | awk '{ print "INSERT INTO default_repos(repo_id) VALUES (" $1 ");" }' | kubectl -n prod exec -i pgsql-767c45c
+# cat 10k_ids | awk '{ print "INSERT INTO default_repos(repo_id) VALUES (" $1 ");" }' | kubectl -n prod exec -i pgsql-767c45c76d-c9278 -- psql -U sg
+cat 10k_ids_pg2 | awk '{ print "INSERT INTO default_repos(repo_id) VALUES (" $1 ");" }' | kubectl -n prod exec -i pgsql-767c45c76d-c9278 -- psql -U sg
