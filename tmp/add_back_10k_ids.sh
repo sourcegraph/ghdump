15a12eb8f10ca99b8d1a129c131ebbd6c9f6246b
Beyang Liu <beyang@sourcegraph.com>
2020-09-12 14:21:19 -0700

    50k_to_100k_ids

--- tmp/add_back_10k_ids.sh
+++ tmp/add_back_10k_ids.sh
@@ -1,3 +1,4 @@
 # cat 10k_ids | awk '{ print "INSERT INTO default_repos(repo_id) VALUES (" $1 ");" }' | kubectl -n prod exec -i pgsql-767c45c76d-c9278 -- psql -U sg
 # cat 10k_ids_pg2 | awk '{ print "INSERT INTO default_repos(repo_id) VALUES (" $1 ");" }' | kubectl -n prod exec -i pgsql-767c45c76d-c9278 -- psql -U sg
-cat 10k_ids_20k_to_50k | awk '{ print "INSERT INTO default_repos(repo_id) VALUES (" $1 ");" }' | kubectl -n prod exec -i pgsql-767c45c76d-c9278 -- psql -U sg
+# cat 10k_ids_20k_to_50k | awk '{ print "INSERT INTO default_repos(repo_id) VALUES (" $1 ");" }' | kubectl -n prod exec -i pgsql-767c45c76d-c9278 -- psql -U sg
+cat 50k_to_100k_ids | awk '{ print "INSERT INTO default_repos(repo_id) VALUES (" $1 ");" }' | kubectl -n prod exec -i pgsql-767c45c76d-c9278 -- psql -U sg
